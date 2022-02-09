7717f111b630b651c814b33e841c1787abc017ae
chasebastian <cbastian@ucsd.edu>
2022-02-08 16:00:01 -0800

    Relay Test

--- Arduino/Relay/Relay_Test/Relay_Test.ino
+++ Arduino/Relay/Relay_Test/Relay_Test.ino
@@ -1,9 +1,18 @@
+  int RelayPin = 10;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(RelayPin, OUTPUT);
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  digitalWrite(RelayPin, HIGH);
+  delay(500);
+  digitalWrite(RelayPin, LOW);
+  delay(500);
 
 }
